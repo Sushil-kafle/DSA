7e7b9a71541d58c818ba52104263dac19c545a86
Sushil-kafle <sushil.kafle@lec.edu.np>
2021-12-16 17:56:50 +0545

    infix to postfix

---
+++ lab/lab_2.2(infix_to_postfix).cpp
@@ -0,0 +1,135 @@
+/*
+
+Algorithms:
+1. append "(" and ")" to the expression.
+2. if "(" is present push it in postfix.
+3. if alphabet is present push it postfix.
+4. if ")" is present, keep popping from stack and push to postfix
+    till "(" is found.
+5.if operator is present,
+    if its precedence is lower or equal than operator of top of stack
+        keep popping from stack and push to postfix
+    else
+        push it to the stack
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int top = -1;
+string postfix = "";
+
+void push(char *stack, char c)
+{
+    top++;
+    stack[top] = c;
+}
+bool contains(string optr, char c)
+{
+    if (c == '+' || c == '-' || c == '*' || c == '/' || c == '$')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+void pop_bracket(char *stack)
+{
+    for (int i = top; i > 0; i--)
+    {
+        if (stack[i] == '(')
+        {
+            top--;
+            return;
+        }
+        postfix += stack[i];
+        top--;
+    }
+}
+int precedence(char a)
+{
+    if (a == '$')
+    {
+        return 3;
+    }
+
+    else if (a == '/' || a == '*')
+    {
+        return 2;
+    }
+    else
+    {
+        return 1;
+    }
+}
+void pop_optr(char *stack, char a)
+{
+    string optr = "+-/*$";
+    int i = top;
+
+    while (contains(optr, stack[i]) && precedence(a) <= precedence(stack[top]))
+    {
+        postfix += stack[top];
+        top--;
+        i--;
+    }
+    push(stack, a);
+    return;
+}
+
+string convert(string infix, char *stack)
+{
+    string optr = "+-/*$";
+    for (int i = 0; i < infix.length(); i++)
+    {
+        if (infix[i] == '(')
+        {
+            push(stack, infix[i]);
+        }
+        else if (infix[i] == ')')
+        {
+            pop_bracket(stack);
+        }
+        else if (contains(optr, infix[i]))
+        {
+            if (contains(optr, stack[top]))
+            {
+                if (precedence(infix[i]) <= precedence(stack[top]))
+                {
+                    pop_optr(stack, infix[i]);
+                }
+                else
+                {
+
+                    push(stack, infix[i]);
+                }
+            }
+            else
+            {
+                push(stack, infix[i]);
+            }
+        }
+        else
+        {
+            postfix += infix[i];
+        }
+    }
+    return postfix;
+}
+
+int main()
+{
+    string infix;
+    char stack[20];
+    cout << "Enter a infix expresion: " << endl;
+    cin >> infix;
+
+    infix = "(" + infix + ")";
+
+    postfix = convert(infix, stack);
+    cout << "The required postfix is: " << endl;
+    cout << postfix << endl;
+}
